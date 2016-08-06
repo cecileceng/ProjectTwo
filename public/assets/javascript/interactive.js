cbc2722c111565c19fe561709a0c7b08c9a06b8c
Lee Warnock <clwarnock@gmail.com>
2016-08-06 11:11:30 -0500

    adds sidenav

--- public/assets/javascript/interactive.js
+++ public/assets/javascript/interactive.js
@@ -1,1 +1,11 @@
 console.log("interactive is linked and ready");
+$('.button-collapse').sideNav({
+  menuWidth: 300, // Default is 240
+  edge: 'left', // Choose the horizontal origin
+  closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+}
+);
+// Show sideNav
+$('.button-collapse').sideNav('show');
+// Hide sideNav
+$('.button-collapse').sideNav('hide');
