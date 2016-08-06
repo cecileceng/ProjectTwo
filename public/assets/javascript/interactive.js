0db451ea7da8bd24bbde397608e3a2ac3b2e2128
Lee Warnock <clwarnock@gmail.com>
2016-08-06 13:03:00 -0500

    changes nav bar to hamburger

--- public/assets/javascript/interactive.js
+++ public/assets/javascript/interactive.js
@@ -1,12 +1,6 @@
 console.log("interactive is linked and ready");
-window.onload = (function($){
-  $(function(){
-
-    $('.button-collapse').sideNav({
-      menuWidth: 300, // Default is 240
-      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
-    });
-
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
-
+//js code
+$(document).ready(function(){
+   $(".button-collapse").sideNav();
+   $('.collapsible').collapsible();
+});
