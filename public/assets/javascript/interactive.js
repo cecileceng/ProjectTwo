1b92be8d0f2003dc412b4ba1984e7e68eec24194
Lee Warnock <clwarnock@gmail.com>
2016-08-06 12:33:50 -0500

    commentes out first nav

--- public/assets/javascript/interactive.js
+++ public/assets/javascript/interactive.js
@@ -1,16 +1,8 @@
 console.log("interactive is linked and ready");
 $('.button-collapse').sideNav({
-  menuWidth: 300, // Default is 240
-  edge: 'left', // Choose the horizontal origin
-  closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
-}
-);
-// Show sideNav
-$('.button-collapse').sideNav({
       menuWidth: 300, // Default is 240
       closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
     }
   );
   $('.collapsible').collapsible();
-  //End Sidenav Menu
 
