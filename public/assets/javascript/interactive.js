f8200778d4bd4884ed613eab8261257559a818a3
Lee Warnock <clwarnock@gmail.com>
2016-08-06 12:48:00 -0500

    changes nav bar to hamburger

--- public/assets/javascript/interactive.js
+++ public/assets/javascript/interactive.js
@@ -1,9 +1,13 @@
 console.log("interactive is linked and ready");
+//Materialize init hack to load menu only after materialize has loaded from CDN
+(function($){
+  $(function(){
 
-$('.button-collapse').sideNav({
+    $('.button-collapse').sideNav({
       menuWidth: 300, // Default is 240
       closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
-    }
-  );
-  $('.collapsible').collapsible();
+    });
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
 
