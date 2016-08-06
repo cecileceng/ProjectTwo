69f26dc7fb646686caba149c7f1f0b6bb221bc25
Cecile Nguyen <cecile.ceng@gmail.com>
2016-08-06 11:35:57 -0500

    adjusting json and server

---
+++ server.js
@@ -0,0 +1,70 @@
+//DEPENDENCIES
+const express = require('express');
+const methodOverride = require('method-override');
+const bodyParser = require('body-parser');
+const app = express();
+const keys = require('./tokens.js');
+
+//OAUTH2
+var oauth2 = require('simple-oauth2')({
+	clientID: CLIENT_ID,
+	clientSecret: CLIENT_SECRET,
+	site: 'https://github.com/login',
+	tokenPath: '/oauth/access_token',
+	authorizationPath: '/oauth/authorize'
+});
+
+// Authorization uri definition
+var authorization_uri = oauth2.authCode.authorizeURL({
+	redirect_uri: 'http://localhost:3000/callback',
+	scope: 'notifications',
+	state: '3(#0/!~'
+});
+
+// Initial page redirecting to Github
+app.get('/auth', function (req, res) {
+	res.redirect(authorization_uri);
+});
+
+// Callback service parsing the authorization token and asking for the access token
+app.get('/callback', function (req, res) {
+	var code = req.query.code;
+
+	oauth2.authCode.getToken({
+		code: code,
+		redirect_uri: 'http://localhost:3000/callback'
+	}, saveToken);
+
+	function saveToken(error, result) {
+		if (error) { console.log('Access Token Error', error.message); }
+		token = oauth2.accessToken.create(result);
+	}
+});
+
+app.get('/', function (req, res) {
+	res.send('Hello<br><a href="/auth">Log in with Github</a>');
+});
+
+//Nothing new added below
+
+//serve up public folder and all content as static files to server.
+app.use(express.static('public'));
+//use bodyParser, do not encode url
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+// override with POST having ?_method=DELETE
+app.use(methodOverride('_method'));
+//require handlebars
+var exphbs = require('express-handlebars');
+//use handlebars engine as template engine, use 'main' as our base file
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+//link to main controller, set as default page"/"
+var routes = require('./controllers/main_controller.js');
+app.use('/', routes);
+//listen on port, if undefined, use 3000
+app.listen(process.env.PORT || 3000);
