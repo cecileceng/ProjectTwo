bb1de9cb6b080f2906b1c045de6cde1bf57f5daa
Michael Desantis <multishifties@yahoo.com>
2016-08-06 13:59:50 -0500

    working chat app, requires styling and config, oauth commented out still

--- server.js
+++ server.js
@@ -3,9 +3,12 @@ const express = require('express');
 const methodOverride = require('method-override');
 const bodyParser = require('body-parser');
 const app = express();
-const keys = require('./tokens.js');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+//const keys = require('./tokens.js');
 
 //OAUTH2
+/*
 var oauth2 = require('simple-oauth2')({
 	clientID: CLIENT_ID,
 	clientSecret: CLIENT_SECRET,
@@ -44,6 +47,7 @@ app.get('/callback', function (req, res) {
 app.get('/', function (req, res) {
 	res.send('Hello<br><a href="/auth">Log in with Github</a>');
 });
+*/
 
 //Nothing new added below
 
@@ -66,5 +70,15 @@ app.set('view engine', 'handlebars');
 //link to main controller, set as default page"/"
 var routes = require('./controllers/main_controller.js');
 app.use('/', routes);
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
 //listen on port, if undefined, use 3000
-app.listen(process.env.PORT || 3000);
+http.listen(process.env.PORT || 3000,function(){
+	process.env.PORT == undefined? console.log("App listening on PORT 3000"):console.log("App listening on PORT" + process.env.PORT);
+});
+
