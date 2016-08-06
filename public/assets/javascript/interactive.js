584c3c9de5550ec4565ada5f4329660ca0d6a1e7
Lee Warnock <clwarnock@gmail.com>
2016-08-06 13:17:46 -0500

    changes nav bar to hamburger

--- public/assets/javascript/interactive.js
+++ public/assets/javascript/interactive.js
@@ -1,6 +1,6 @@
 console.log("interactive is linked and ready");
 //js code
-$(document).ready(function(){
+/*$(document).ready(function(){
  // Initialize collapse button
   $(".button-collapse").sideNav({
       menuWidth: 300, // Default is 240
@@ -9,4 +9,13 @@ $(document).ready(function(){
     });
   // Initialize collapsible (uncomment the line below if you use the dropdown variation)
   $('.collapsible').collapsible();
-});
+});*/
+
+
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
