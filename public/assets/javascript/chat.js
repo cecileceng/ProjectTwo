5761ef94c53aeb2e85783fef89e1ef846c430c83
Michael Desantis <multishifties@yahoo.com>
2016-08-09 16:38:51 -0500

    chat message limiter in place

--- public/assets/javascript/chat.js
+++ public/assets/javascript/chat.js
@@ -1,10 +1,24 @@
+var socket = io();
+$('form').submit(function() {
+    socket.emit('chat message', $('#m').val());
+    $('#m').val('');
+    return false;
+});
 
-      var socket = io();
-      $('form').submit(function(){
-        socket.emit('chat message', $('#m').val());
-        $('#m').val('');
-        return false;
-      });
-      socket.on('chat message', function(msg){
-        $('#messages').append($('<li>').text(msg));
-      });
+//function for child length function of message UL.
+function removeOldListItem() {
+	var messageChild = $('#messages').children().length;
+    //test length.
+    //if more than 6, remove first message before appending new message to bottom.
+    if (messageChild>5){
+    	var list = document.getElementById("messages");
+    	list.removeChild(list.childNodes[0]);
+    }
+    console.log(messageChild);
+}
+
+//chat function. append new message to messages ul element
+socket.on('chat message', function(msg) {
+	removeOldListItem();
+    $('#messages').append($('<li>').text(msg));
+});
