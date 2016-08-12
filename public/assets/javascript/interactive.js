8fb34a643fb32fe54e3a5c6dc7a783255eadafa1
Cecile Nguyen <cecile.ceng@gmail.com>
2016-08-12 18:41:55 -0500

    adjusting interactive again

--- public/assets/javascript/interactive.js
+++ public/assets/javascript/interactive.js
@@ -8,10 +8,9 @@ $(document).ready(function(){
       menuWidth: 300, // Default is 240
       edge: 'left', // Choose the horizontal origin
       closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
-        // Initialize collapsible (uncomment the line below if you use the dropdown variation)
-  $('.collapsible').collapsible();
     });
-
+  // Initialize collapsible (uncomment the line below if you use the dropdown variation)
+  $('.collapsible').collapsible();
 
 
 });//end document.ready
