10b2d88a939d73a4e5a2d0efbfa3b54f56cd8618
Cecile Nguyen <cecile.ceng@gmail.com>
2016-08-12 18:16:35 -0500

    adjusted syntax in interactiveJS

--- public/assets/javascript/interactive.js
+++ public/assets/javascript/interactive.js
@@ -2,11 +2,28 @@ $(document).ready(function(){
   console.log("interactive is linked and ready");
 
  // Initialize collapse button
-$('.button-collapse').sideNav({
-  menuWidth: 240, // Default is 240
-  closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
-});
-$('.collapsible').collapsible();
+
+
+  $(".button-collapse").sideNav({
+      menuWidth: 300, // Default is 240
+      edge: 'left', // Choose the horizontal origin
+      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+        // Initialize collapsible (uncomment the line below if you use the dropdown variation)
+  $('.collapsible').collapsible();
+    });
 
 
 
+});//end document.ready
+
+
+
+/*
+
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space*/
