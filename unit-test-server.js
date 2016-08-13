c2300229f4f9ede2f58ec9d1a0ae3bd78d3244d3
Cecile Nguyen <cecile.ceng@gmail.com>
2016-08-12 20:14:30 -0500

    adjusting config

---
+++ unit-test-server.js
@@ -0,0 +1,9 @@
+var models = require('./models');
+const keys = require('./tokens.js');
+tokens = require('./tokens.js');
+
+var profile = {githubID: "testuser", 
+               name: "no", 
+               email: "no@no.com", 
+               userName: "userno"};
+console.log(profile);
