dc0f6c494e30666dabcad37433011a550fa22911
Lee Warnock <clwarnock@gmail.com>
2016-08-13 11:41:01 -0500

    adding my env

--- tokens.js
+++ tokens.js
@@ -1,8 +1,8 @@
-var Client_ID = "d34e37706529a775c0b1";
+var Client_ID = "70f33de858011e5b0829";
 
-var Client_Secret = "ad1201bab5eedbb9fc699d27a9cdbad377f1b7f1";
+var Client_Secret = "2a9ab9afee112e1554b5edfe0629261b4af94c74";
 
-var Personal_Token = "49943baef84a0ca83f657b5e150b9f79e42ced59";
+var Personal_Token = "5b59b12ec6642f9101a04d57f68a9f2043ce604c";
 
 exports.Client_ID = Client_ID;
 exports.Client_Secret = Client_Secret;
--- unit-test-server.js
+++ unit-test-server.js
@@ -3,8 +3,8 @@ var models = require('./models');
 const keys = require('./tokens.js');
 tokens = require('./tokens.js');
 
-var profile = {githubID: "testuser", 
-               name: "no", 
-               email: "no@no.com", 
+var profile = {githubID: "testuser",
+               name: "no",
+               email: "no@no.com",
                userName: "userno"};
 console.log(profile);
