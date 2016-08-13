497587e69d2421d312d5829a0f6afed6861f0228
Lee Warnock <clwarnock@gmail.com>
2016-08-13 00:56:54 -0500

    adds map marker image to maps.js

--- public/assets/javascript/maps.js
+++ public/assets/javascript/maps.js
@@ -75,7 +75,7 @@ for(i=0; i<totalNumberOfMarkers; i++){
 
 	//new instance of google marker
 	var aWildMarker = new google.maps.Marker({
-			icon: "../public/assets/img/marker.png",
+			icon: "marker.png",
             position: locationObjectForMarker,
             map: map, //targets var map, which tells program which map to add marker to.
             title: theCurrentTitleText, //appears if you hover over marker
