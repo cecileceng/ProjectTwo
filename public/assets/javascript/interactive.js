6df7d9720920fffaa3ae927496ea212a7289bd54
Lee Warnock <clwarnock@gmail.com>
2016-08-12 20:09:14 -0500

    adds list styling

--- public/assets/javascript/interactive.js
+++ public/assets/javascript/interactive.js
@@ -6,11 +6,10 @@ $(document).ready(function(){
 
   $(".button-collapse").sideNav({
       menuWidth: 240, // Default is 240
-      edge: 'left', // Choose the horizontal origin
       closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
     });
   // Initialize collapsible (uncomment the line below if you use the dropdown variation)
-  //$('.collapsible').collapsible();
+  $('.collapsible').collapsible();
 
 
 });//end document.ready
