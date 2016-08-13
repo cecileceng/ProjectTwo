c2f3ff2b58e28ad25ad1bf20344c1323760fde84
Lee Warnock <clwarnock@gmail.com>
2016-08-12 19:08:02 -0500

    adds mobile mockups

--- public/assets/javascript/interactive.js
+++ public/assets/javascript/interactive.js
@@ -2,11 +2,18 @@ $(document).ready(function(){
   console.log("interactive is linked and ready");
 
  // Initialize collapse button
-$('.button-collapse').sideNav({
-  menuWidth: 240, // Default is 240
-  closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
-});
-$('.collapsible').collapsible();
+
+
+  $(".button-collapse").sideNav({
+      menuWidth: 240, // Default is 240
+      edge: 'left', // Choose the horizontal origin
+      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    });
+  // Initialize collapsible (uncomment the line below if you use the dropdown variation)
+  //$('.collapsible').collapsible();
+
+
+});//end document.ready
 
 
 
